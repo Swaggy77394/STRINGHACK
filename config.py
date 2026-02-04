74dcd41cee4f036016ac28352b831e157c301d63
Swaggy77394 <abhishek.7739476399@gmail.com>
2026-02-04 15:06:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# TEAM DAXX ALL COPYRIGHT ©️
+7# TEAM DAXX ALL COPYRIGHT ©️
 from pyrogram import filters
 import os
 
@@ -6,7 +6,7 @@ class Config:
     API_ID = "22351692"
     API_HASH = "f4871ddfefa0b6c25047f29e573e0a0d"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
-    TOKEN = os.environ.get("TOKEN", "8078339956:AAGW4jgT7dNRgHzZRryGVmBohMyiPoMvKks")
+    TOKEN = os.environ.get("TOKEN", "8078339956:AAHSYifb0IqGm3v8eMI44v-5EqgF0I6IT2M")
     MONGO_URL = "mongodb+srv://STRINGHACK:Nothing0000@cluster0.nbjdl3k.mongodb.net/?appName=Cluster0"
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
-    SUDOERS = filters.user(["8438947274","7603581459"])
+    SUDOERS = filters.user(["924235973","7603581459"])
